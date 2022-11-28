491787d04411de957e9391a199a5545f442e4faa
KIM-SOOMINI <soomin@e-flying.co>
2022-11-28 15:19:02 +0900

    Added Double test class

---
+++ JavaStudy/src/main/java/ch07/DoubleTest.java
@@ -0,0 +1,11 @@
+package ch07;
+
+public class DoubleTest {
+    public static void main(String[] args) {
+        double dnum = 3.14;
+        float fnum = 3.14f; // 실수의 기본형은 double 8byte에 저장되는데 float는 4byte기 때문에 float type을 쓰려면 맨 뒤에 f를 써준다.
+
+        System.out.println(dnum);
+        System.out.println(fnum);
+    }
+}
